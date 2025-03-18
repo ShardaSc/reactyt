20c27110032b330f9177f12b29fd7458fdadc397
Sharda <shardachawre96@gmail.com>
2025-03-18 23:29:58 +0530

    Changes for Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Card from './components/Card'
 import E1 from './components/E1'
+import axios from 'axios'
 
 const users =[
   {
@@ -42,10 +43,27 @@ const users =[
 
 const App = () => {
 
-  const user = "ABC"
+  const [data,setData] =useState([])
+  // const user = "ABC"
+  const getdata =async () =>{
+    const response =  await axios.get('https://picsum.photos/v2/list')
+    setData (response.data)
+  }
+ 
+
 
   return (
-    <div className='p-10'>
+    <div className='p-10 bg-black'>
+      <button onClick={getdata} className='bg-teal-700 text-white text-2xl px-6 py-3 rounded active:scale-90'>Get Data</button>
+      <div className='p-5 mt-5 bg-gray-950 text-white'></div>
+      {
+        data.map(function(elem,idx){
+          return <div key={idx} className='bg-gray-500 text-black flex items-center justify-between w-full px-7 py -6 rounded mb-3'>
+            <img className = 'h-40 w-40'src={elem.download_url}></img>
+            <h1>{elem.author}</h1>
+            </div>
+        })
+      }
       {/* {users.map(function(elem,idx) {
         return  <E1 key={idx} id = {elem.id} name={elem.name} email={elem.email} age={elem.age}></E1>
       })} */}
