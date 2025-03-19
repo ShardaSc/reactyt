0b8b6087b092ad67fcb5ddac67be9211520fcdd7
Sharda <shardachawre96@gmail.com>
2025-03-19 22:41:23 +0530

    Routing in React js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import Card from './components/Card'
 import E1 from './components/E1'
 import axios from 'axios'
+import { Route, Routes } from 'react-router-dom'
+import AboutUs from './pages/AboutUs'
 
 const users =[
   {
@@ -55,22 +57,26 @@ const App = () => {
 
 
   return (
-    <div className='p-10 bg-black'>
+    <div >
       {/* <button onClick={getdata} className='bg-teal-700 text-white text-2xl px-6 py-3 rounded active:scale-90'>Get Data</button> */}
-      <div className='p-5 mt-5 bg-gray-950 text-white'></div>
-      {
+      {/* <div className='p-5 mt-5 bg-gray-950 text-white'></div> */}
+      <Routes>
+        <Route path='/about' element= {<AboutUs />}/>
+      </Routes>
+      {/* {
         data.map(function(elem,idx){
           return <div key={idx} className='bg-gray-500 text-black flex items-center justify-between w-full px-7 py -6 rounded mb-3'>
             <img className = 'h-40 w-40'src={elem.download_url}></img>
             <h1>{elem.author}</h1>
             </div>
         })
-      }
+      } */}
       {/* {users.map(function(elem,idx) {
         return  <E1 key={idx} id = {elem.id} name={elem.name} email={elem.email} age={elem.age}></E1>
       })} */}
       {/* <E1 user ={user}></E1>  props drilling*/}
     </div>
+
   )
 }
 
